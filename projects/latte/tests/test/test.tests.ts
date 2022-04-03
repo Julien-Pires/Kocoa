477d0c6d9f8e1345213bff5cb94a031a00f1b530
Julien Pires <piresjulien95@gmail.com>
2022-04-03 23:21:56 +0200

    Rename some tests

--- projects/latte/tests/test/test.tests.ts
+++ projects/latte/tests/test/test.tests.ts
@@ -5,7 +5,7 @@ import { test, TestAnnotation, testData, testSuite, testSymbol } from '../../src
 @testSuite('@test')
 export class TestDecoratorTests {
     @test
-    @testData({ testName: 'should add test metadata when method is decorated' })
+    @testData({ testName: 'should add test metadata when method is annoted' })
     public shouldAddMetadata() {
         const actual: TestAnnotation = Reflect.getMetadata(testSymbol, this, this.shouldAddMetadata.name);
 
@@ -13,7 +13,7 @@ export class TestDecoratorTests {
     }
 
     @test
-    @testData({ testName: 'should contains method name when method is decorated' })
+    @testData({ testName: 'should contains method name when method is annoted' })
     public shouldContainsMethodName() {
         const actual: TestAnnotation = Reflect.getMetadata(testSymbol, this, this.shouldContainsMethodName.name);
 
@@ -21,7 +21,7 @@ export class TestDecoratorTests {
     }
 
     @test
-    @testData({ testName: 'should contains method body when method is decorated' })
+    @testData({ testName: 'should contains method body when method is annoted' })
     public shouldContainsMethod() {
         const actual: TestAnnotation = Reflect.getMetadata(testSymbol, this, this.shouldContainsMethod.name);
 
