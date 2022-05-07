b31d599291e436574455e3d21e04e005e0dbef74
Julien Pires <piresjulien95@gmail.com>
2022-05-07 23:14:43 +0200

    fix: first espresso update

--- apps/espresso/src/discovery.ts
+++ apps/espresso/src/discovery.ts
@@ -11,6 +11,8 @@ import {
 import { createTree, insertLeaf, insertNodes, Test, TestSuite, Tree } from './types/index.js';
 
 
+
+
 /**
  * Checks if specified property is a test (has test metadata).
  * @param target Target parent of the property.
