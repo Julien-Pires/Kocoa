bd073376a54a3b807d814d46cb40b81f0eb48a86
Julien Pires <piresjulien95@gmail.com>
2022-05-07 23:21:56 +0200

    refactor: remove espresso

--- apps/espresso/src/annotations/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './metadata.js';
-export * from './reflect.js';
-export * from './suite.js';
-export * from './testData.js';
-export * from './test.js';
-export * from './types.js';
--- apps/espresso/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './annotations/index.js';
-
-
