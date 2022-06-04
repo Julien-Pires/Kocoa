49e447eefde9d8208d6a6c3fab4cbf8de5097c5b
Julien Pires <piresjulien95@gmail.com>
2022-06-04 18:29:14 +0200

    test: refactor test suite and member data tests

--- apps/kocoa/tests/memberData.fixtures.ts
+++ apps/kocoa/tests/memberData.fixtures.ts
@@ -14,84 +14,82 @@ class DataClass<T> implements Iterable<T> {
 }
 
 export class MemberDataFixture {
-    static readonly empty: [] = [];
+    static readonly emptyDataSource: [] = [];
 
-    static readonly oddNumber: number[][] = [
+    static readonly arrayDataSource: number[][] = [
         [1, 3, 4],
         [100, 300, 400],
         [1000, 3000, 4000]
     ];
 
-    static readonly evenNumber = new DataClass([
+    static readonly iterableDataSource = new DataClass([
         [2, 4, 6],
         [200, 400, 600],
         [2000, 4000, 6000]
     ]);
 
     /* eslint-disable @typescript-eslint/no-empty-function */ //
-    static *getEmpty() {}
+    static *emptyIterableDataSource() {}
 
-    static getOddNumber(): number[][] {
-        return [
-            [1, 3, 4],
-            [100, 300, 400],
-            [1000, 3000, 4000]
-        ];
+    static arrayMethodDataSource(): number[][] {
+        return MemberDataFixture.arrayDataSource;
     }
 
-    static *getEvenNumber() {
-        yield [2, 4, 6];
-        yield [200, 400, 600];
-        yield [2000, 4000, 6000];
+    static *iterableMethodDataSource() {
+        yield [4, 2, 2];
+        yield [400, 200, 200];
+        yield [4000, 2000, 2000];
     }
 
-    static *multiplyByTwo(number: number) {
-        yield [number, number * 2];
+    static *parameterizedDataSource(add: number) {
+        yield [1 + add, 3 + add, 4 + add];
+        yield [100 + add, 300 + add, 400 + add];
+        yield [1000 + add, 3000 + add, 4000 + add];
     }
 
-    public noMemberData() {
+    public noDataSourceTest() {
         return true;
     }
 
-    @memberData(MemberDataFixture.empty)
-    @memberData(MemberDataFixture.getEmpty)
-    public emptyMemberDataTest() {
+    @memberData(MemberDataFixture.emptyDataSource)
+    @memberData(MemberDataFixture.emptyIterableDataSource)
+    public emptyDataSourceTest() {
         return true;
     }
 
-    @memberData(MemberDataFixture.oddNumber)
-    public singleFieldDataTest(first: number, second: number, expected: number) {
+    @memberData(MemberDataFixture.arrayDataSource)
+    public singleDataSourceTest(first: number, second: number, expected: number) {
         return first + second == expected;
     }
 
-    @memberData(MemberDataFixture.evenNumber)
-    @memberData(MemberDataFixture.oddNumber)
-    public multiFieldDataTest(first: number, second: number, expected: number) {
+    @memberData(MemberDataFixture.arrayDataSource)
+    @memberData(MemberDataFixture.arrayMethodDataSource)
+    public multipleDataSourceTest(first: number, second: number, expected: number) {
         return first + second == expected;
     }
 
-    @memberData(MemberDataFixture.oddNumber)
-    public arrayFieldDataTest(first: number, second: number, expected: number) {
+    @memberData(MemberDataFixture.arrayDataSource)
+    public arrayDataSourceTest(first: number, second: number, expected: number) {
         return first + second == expected;
     }
 
-    @memberData(MemberDataFixture.evenNumber)
-    public iterableFieldDataTest(first: number, second: number, expected: number) {
+    @memberData(MemberDataFixture.iterableDataSource)
+    public iterableDataSourceTest(first: number, second: number, expected: number) {
         return first + second == expected;
     }
 
-    @memberData(MemberDataFixture.getOddNumber)
-    public arrayMethodDataTest(first: number, second: number, expected: number) {
+    @memberData(MemberDataFixture.arrayMethodDataSource)
+    public arrayMethodDataSourceTest(first: number, second: number, expected: number) {
         return first + second == expected;
     }
 
-    @memberData(MemberDataFixture.getEvenNumber)
-    public iterableMethodDataTest(first: number, second: number, expected: number) {
+    @memberData(MemberDataFixture.iterableMethodDataSource)
+    public iterableMethodDataSourceTest(first: number, second: number, expected: number) {
         return first + second == expected;
     }
 
-    @memberData(MemberDataFixture.multiplyByTwo, 2)
-    public checkEmailTest(initial: number, result: number) {
-        return initial * 2 === result;
+    @memberData(MemberDataFixture.parameterizedDataSource, 2)
+    public parameterizedDataSourceTest(first: number, second: number, expected: number) {
+        return first + second == expected;
     }
 }
--- apps/kocoa/tests/suite.tests.ts
+++ apps/kocoa/tests/suite.tests.ts
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import chaiSubset from 'chai-subset';
 
-import { suite, test } from '../index.js';
+import { suite, test, testData } from '../index.js';
 import { suiteSymbol } from '../src/metadata.js';
 import * as Reflect from '../src/reflect.js';
 import { MultipleSuiteFixture, SingleSuiteFixture, SkipOptionsFixtures } from './suite.fixtures.js';
@@ -13,108 +13,79 @@ chai.use(chaiSubset);
 
 @suite('@suite')
 export class TestSuiteClassDecoratorTests {
-    @test({ name: 'should contains suite metadata when class has one annotation' })
-    @suite('class')
-    public classShouldContainsSingleSuiteWhenAnnoted() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, SingleSuiteFixture);
+    @test
+    @testData(SingleSuiteFixture, 1)
+    @testData(MultipleSuiteFixture, 3)
+    public 'should contains test suite annotation when class is annoted'(ctor: object, expected: number) {
+        const actual = Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, ctor);
 
         expect(actual).to.not.be.undefined;
-        expect(actual).to.be.of.length(1);
+        expect(actual).to.be.of.length(expected);
     }
 
-    @test({ name: 'should contains mutliple suite metadata when class has multiple annotation' })
-    @suite('class')
-    public classShouldContainsMultipleSuiteWhenAnnoted() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, MultipleSuiteFixture);
+    @test
+    @testData(SingleSuiteFixture, [{ name: 'testSuite' }])
+    @testData(MultipleSuiteFixture, [{ name: 'testSuite' }, { name: 'sample' }, { name: 'class' }])
+    public 'should have specified name for test suite annotation on class'(ctor: object, expected: { name: string }[]) {
+        const actual = Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, ctor);
+
+        expect(actual).to.containSubset(expected);
+    }
+
+    @test
+    @testData(MultipleSuiteFixture.prototype, MultipleSuiteFixture.prototype.singleTestSuite.name, 1)
+    @testData(MultipleSuiteFixture.prototype, MultipleSuiteFixture.prototype.multipleTestSuite.name, 3)
+    public 'should contains test suite annotation when method is annoted'(
+        ctor: object,
+        methodName: string,
+        expected: number
+    ) {
+        const actual = Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, ctor, methodName);
 
         expect(actual).to.not.be.undefined;
-        expect(actual).to.be.of.length(3);
+        expect(actual).to.be.of.length(expected);
     }
 
-    @test({ name: 'should contains correct suite name when class has annotations' })
-    @suite('class')
-    public classShouldContainsSuiteNameWhenAnnoted() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, MultipleSuiteFixture);
+    @test
+    @testData(MultipleSuiteFixture.prototype, MultipleSuiteFixture.prototype.singleTestSuite.name, [
+        { name: 'testSuite' }
+    ])
+    @testData(MultipleSuiteFixture.prototype, MultipleSuiteFixture.prototype.multipleTestSuite.name, [
+        { name: 'testSuite' },
+        { name: 'sample' },
+        { name: 'method' }
+    ])
+    public 'should have specified name for test suite annotation on method'(
+        ctor: object,
+        methodName: string,
+        expected: { name: string }[]
+    ) {
+        const actual = Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, ctor, methodName);
 
-        expect(actual).to.containSubset([{ name: 'testSuite' }, { name: 'sample' }, { name: 'class' }]);
+        expect(actual).to.containSubset(expected);
     }
 
-    @test({ name: 'should contains test suite metadata when method has one annotation' })
-    @suite('method')
-    public methodShouldContainsSingleSuiteWhenAnnoted() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(
-            suiteSymbol,
-            MultipleSuiteFixture.prototype,
-            MultipleSuiteFixture.prototype.singleTestSuite.name
-        );
+    @test
+    @testData(SkipOptionsFixtures)
+    @testData(SkipOptionsFixtures.prototype, SkipOptionsFixtures.prototype.skippedMethod.name)
+    public 'should have skip option set to true when skip options is set to true'(ctor: object, methodName?: string) {
+        const actual = methodName
+            ? Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, ctor, methodName)
+            : Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, ctor);
 
-        expect(actual).to.not.be.undefined;
-        expect(actual).to.be.of.length(1);
-    }
-
-    @test({ name: 'should contains multiple test suite metadata when method has multiple annotation' })
-    @suite('method')
-    public methodShouldContainsMultipleSuiteWhenAnnoted() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(
-            suiteSymbol,
-            MultipleSuiteFixture.prototype,
-            MultipleSuiteFixture.prototype.multipleTestSuite.name
-        );
-
-        expect(actual).to.not.be.undefined;
-        expect(actual).to.be.of.length(3);
-    }
-
-    @test({ name: 'should contains correct suite name when method has annotations' })
-    @suite('method')
-    public methodShouldContainsSuiteNameWhenAnnoted() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(
-            suiteSymbol,
-            MultipleSuiteFixture.prototype,
-            MultipleSuiteFixture.prototype.multipleTestSuite.name
-        );
-
-        expect(actual).to.containSubset([{ name: 'testSuite' }, { name: 'sample' }, { name: 'method' }]);
-    }
-
-    @test({ name: 'should have skip options to true when skip options is used' })
-    @suite('class')
-    public shouldHaveSkipOptionsOnClassWhenSpecified() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, SkipOptionsFixtures);
-
-        expect(actual[0].options.skip).to.not.be.undefined;
         expect(actual[0].options.skip).to.be.true;
     }
 
-    @test({ name: 'should have skip options to false when skip options is not used' })
-    @suite('class')
-    public shouldNotHaveSkipOptionsOnClassWhenNotSpecified() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, SingleSuiteFixture);
-
-        expect(actual[0].options.skip).to.be.false;
-    }
-
-    @test({ name: 'should have skip options to true when skip options is used' })
-    @suite('method')
-    public shouldHaveSkipOptionsOnMethodWhenSpecified() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(
-            suiteSymbol,
-            SkipOptionsFixtures.prototype,
-            SkipOptionsFixtures.prototype.skippedMethod.name
-        );
-
-        expect(actual[0].options.skip).to.not.be.undefined;
-        expect(actual[0].options.skip).to.be.true;
-    }
-
-    @test({ name: 'should have skip options to false when skip options is not used' })
-    @suite('method')
-    public shouldNotHaveSkipOptionsOnMethodWhenNotSpecified() {
-        const actual = Reflect.getAllMetadata<SuiteAnnotation>(
-            suiteSymbol,
-            MultipleSuiteFixture.prototype,
-            MultipleSuiteFixture.prototype.singleTestSuite.name
-        );
+    @test
+    @testData(SingleSuiteFixture)
+    @testData(MultipleSuiteFixture.prototype, MultipleSuiteFixture.prototype.singleTestSuite.name)
+    public 'should have skip option set to false when skip options is not specified'(
+        ctor: object,
+        methodName?: string
+    ) {
+        const actual = methodName
+            ? Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, ctor, methodName)
+            : Reflect.getAllMetadata<SuiteAnnotation>(suiteSymbol, ctor);
 
         expect(actual[0].options.skip).to.be.false;
     }
