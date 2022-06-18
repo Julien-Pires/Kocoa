ad3ba9b451620562faa0b4f89ffd1af10ef9b4a4
Julien Pires <piresjulien95@gmail.com>
2022-06-18 18:53:21 +0200

    refactor(@kocoa/adapter-mocha): enforce test func constraint on specified target

--- packages/kocoa-adapter-mocha/src/spec.ts
+++ packages/kocoa-adapter-mocha/src/spec.ts
@@ -3,23 +3,27 @@ import { Test } from 'mocha';
 /**
  * Represents test informations.
  */
-export interface SpecInfo {
+export interface SpecInfo<TFunc extends string | symbol | number> {
     name: string;
-    function: string | symbol;
+    function: TFunc;
     skip: boolean;
 }
 
 /**
  * Represents a Mocha test to run with specified test data.
  */
-export class MochaSpec extends Test {
+export class MochaSpec<TTarget extends object, TFunc extends keyof TTarget> extends Test {
     /**
      * Constructor of MochaSpec class.
      * @param spec Informations about the current test.
      * @param data Test data to use when running the test.
      * @param target Prototype of the class that owns this test.
      */
-    constructor(private readonly spec: SpecInfo, private readonly data: unknown[], private readonly target: object) {
+    constructor(
+        private readonly spec: SpecInfo<TFunc>,
+        private readonly data: unknown[],
+        private readonly target: TTarget
+    ) {
         super(spec.name);
 
         this.pending = spec.skip;
