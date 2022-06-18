9cd95911c9114bcf245945712dd9bd92665b0185
Julien Pires <piresjulien95@gmail.com>
2022-06-18 13:00:18 +0200

    feat(@kocoa/types): enforce spec target to extend `object`

--- packages/kocoa-types/src/spec.ts
+++ packages/kocoa-types/src/spec.ts
@@ -1,8 +1,10 @@
+export type SpecData = unknown[];
+
 export interface Spec<TFunc extends string | symbol> {
     name: string;
     function: TFunc;
     skip: boolean;
-    data: () => Iterable<unknown[]>;
+    data: () => Iterable<SpecData>;
 }
 
 export interface Suite {
